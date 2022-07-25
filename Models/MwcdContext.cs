d4de697a3ab651728d1288017fd656293ab4384a
yashjain <yashjain09488@gmail.com>
2022-07-25 22:04:02 +0530

    Tnitial Commit

---
+++ Models/MwcdContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Women_Empowerment.Models
+{
+    public class MwcdContext:DbContext
+    {
+        public MwcdContext(DbContextOptions <MwcdContext> options):base(options)
+        {
+
+        }   
+
+
+    }
+}
